d5fa54b89e953af347a41d9025ea17dcda72a4ed
Trevor Situ <trevorsitu8@gmail.com>
2022-08-17 00:18:46 -0700

    added display of real time results and a final score after one competitor reaches 5 wins

--- rps.js
+++ rps.js
@@ -12,19 +12,18 @@ function getComputerChoice (){
 
 
 function gameplay (playerSelection, pcSelection) {
-    let playerChoice = playerSelection.toLowerCase();
-
-    if (playerChoice === pcSelection) {
+    
+    if (playerSelection === pcSelection) {
         return "Tie! You both chose " + pcSelection;
-    } else if (playerChoice === 'rock' && pcSelection === 'paper') {
+    } else if (playerSelection === 'rock' && pcSelection === 'paper') {
         return "You lose! rock loses to paper";
-    } else if (playerChoice === 'paper' && pcSelection === 'rock') {
+    } else if (playerSelection === 'paper' && pcSelection === 'rock') {
         return "Congrats! rock beats paper"
-    } else if (playerChoice === 'scissors' && pcSelection === 'rock') {
+    } else if (playerSelection === 'scissors' && pcSelection === 'rock') {
         return "You lose! scissors loses to rock";
-    } else if (playerChoice === 'scissors' && pcSelection === 'paper') {
+    } else if (playerSelection === 'scissors' && pcSelection === 'paper') {
         return "Congrats! scissors beats paper";
-    } else if (playerChoice === 'rock' && pcSelection === 'scissors') {
+    } else if (playerSelection === 'rock' && pcSelection === 'scissors') {
         return "Congrats! rock beats scissors";
     } else {
         return "You lose! paper loses to scissors";
@@ -36,31 +35,44 @@ function game() {
     let pcScore = 0;
     let gamerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
+    const rock = document.getElementById("rock");
+    const paper = document.getElementById("paper");
+    const scissors = document.getElementById("scissors");
 
-        let ai = getComputerChoice();
-        let gamer = prompt("Rock, paper or scissors?");
+    
 
-        let result = gameplay(gamer, ai);
-        console.log(result);
+    // add these ^ event listeners into the 5 round for-loop and 2 children 'divs' of .scoreboard that display each persons current score?
+    while (pcScore < 5 && gamerScore < 5) {
+
+        rock.addEventListener("click", function() {
+            gameplay("rock", getComputerChoice());
+        });
+    
+        paper.addEventListener("click", function() {
+            gameplay("paper", getComputerChoice());
+        });
+    
+        scissors.addEventListener("click", function() {
+            gameplay("scissor", getComputerChoice())
+        });
         
-        let char = result.charAt(0);
-
-        if (char === 'C') {
-            gamerScore++;
-        } else if (char === 'Y') {
-            pcScore++;
-        }
-
+        
     }
 
     if (pcScore > gamerScore) {
         console.log("After 5 rounds, you've lost " + pcScore + " to " + gamerScore);
+
+
     } else if (gamerScore > pcScore) {
         console.log("After 5 rounds, you've won " + gamerScore + " to " + pcScore);
     } else {
         console.log("After 5 rounds, you've tied " + pcScore + " to " + gamerScore);
     }
 }
+const container = document.querySelector('#container');
+const div = document.createElement('div');
 
+div.classList.add("Scoreboard");
+
+container.appendChild(div);
 game();
